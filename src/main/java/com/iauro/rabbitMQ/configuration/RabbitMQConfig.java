9bb7d03eaeddeab7690bdfbbd289d032553d1820
LaxmanM13 <laxmanmule13@gmail.com>
2022-05-09 10:23:34 +0530

    first version

---
+++ src/main/java/com/iauro/rabbitMQ/configuration/RabbitMQConfig.java
@@ -0,0 +1,56 @@
+package com.iauro.rabbitMQ.configuration;
+
+
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.context.annotation.Bean;
+
+public class RabbitMQConfig {
+
+
+    public static final String ROUNTING_A=routing.A;
+    public static final String  ROUTING_B=routing.B;
+    @congiguration
+    public class RabbitMQConfig {
+        @Bean
+        Queue queueA() {
+            return new Queue();
+        }
+        @Bean
+        Queue queueB()
+        {
+            return new Queue();
+        }
+
+        @Bean
+        DirectExchange exchange() {
+            return new DirectExchange();
+        }
+
+
+        @Bean
+        Binding binding(Queue queueA, DirectExchnage exchnage) {
+            return BindingBuilder.bind(queueA)
+                    .to(exchange)
+                    .with(ROUTING_A);
+        }
+
+        @Bean
+        Binding binding(Queue queueB,DirectExchange exchange)
+        {
+            return BindingBuilder.bind(QueueB)
+                    .to(exchange)
+                    .with(ROUTING_B);
+        }
+
+        @Bean
+        RabbitTemplate rabbitTemplate(ConnectionFactory factory) {
+            RabbitTemplate rabbitTemplate = new RabbitTemplate(factory);
+            rabbitTemplate.setMessageConverter(messageConverter());
+            return rabbitTemplate;
+        }
+
+
+    }
+}
+
